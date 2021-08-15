49f8ced15444994a054419563ef579c6428e8001
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-08-15 20:03:14 +0700

    BT5

---
+++ Chap 3/BT5.js
@@ -0,0 +1,23 @@
+const number = 1;
+
+function sin(number) {
+    const result = Math.sin(number);
+    console.log(result);
+    return result;
+};
+
+function cos(number) {
+    const result =  Math.cos(number);
+    console.log(result);
+    return result;
+}
+
+function tan(number) {
+    const result = Math.tan(number);
+    console.log(result);
+    return result;
+}
+
+sin(number);
+cos(number);
+tan(number);
